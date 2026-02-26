469e7866e59609b93d22b67cb7791483da24a694
Hemalatha267 <bandarihemalatha37@gmail.com>
2026-02-26 14:48:38 +0530

    did the operations

---
+++ script.js
@@ -0,0 +1,22 @@
+let obj1={
+    name:'Hema',
+    age:19
+}
+let obj2=obj1
+obj2.name='Latha';
+console.log(obj1.name)
+if(1){
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+console.log(5+'5');
+console.log(5-'5');
+console.log(5=='5');
+console.log(5==='5');
+
+
+
+let arr=[1,'new',2,'3']
+console.log(arr);
